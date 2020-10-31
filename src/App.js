cedeeeaa04ff8f694e95c9cc48386c4119117b49
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2020-10-31 03:23:14 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import './styles/App.scss';
+
+const App = () => {
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const handleButtonClick = () => {
+    if (prefersDarkScheme && prefersDarkScheme.matches) {
+      document.body.classList.toggle("light-theme");
+    } else {
+      document.body.classList.toggle("dark-theme");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="toggleWrapper">
+      <input type="checkbox" className="dn" id="dn"/>
+      <label for="dn" className="toggle" onClick={handleButtonClick} >
+        <span className="toggle__handler">
+          <span className="crater crater--1"></span>
+          <span className="crater crater--2"></span>
+          <span className="crater crater--3"></span>
+        </span>
+        <span className="star star--1"></span>
+        <span className="star star--2"></span>
+        <span className="star star--3"></span>
+        <span className="star star--4"></span>
+        <span className="star star--5"></span>
+        <span className="star star--6"></span>
+      </label>
     </div>
   );
 }
